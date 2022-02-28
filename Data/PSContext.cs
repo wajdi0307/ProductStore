66853ed49b9c3bf7f00039b63d40f6a70698f21d
wajdi0307 <78967021+wajdi0307@users.noreply.github.com>
2022-02-28 11:00:14 +0100

    DB Commit

--- Data/PSContext.cs
+++ Data/PSContext.cs
@@ -2,9 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using PS.Domains;
+
 namespace Data
 {
   public class PSContext:DbContext
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chemical> Chemicals { get; set; }
+        public DbSet<Biological> biologicals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+                                        Initial Catalog=ProductStoreDB;
+                                        Integrated Security=true;
+                                        MultipleActiveResultSets=true");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
